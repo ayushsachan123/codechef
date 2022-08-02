c3a05302b083f7bcdd506e02b2b7ead0208a6325
ayushsachan123 <97084090+ayushsachan123@users.noreply.github.com>
2022-08-02 18:52:54 +0530

    Create CBSPEED.java

---
+++ START41D/CBSPEED.java
@@ -0,0 +1,30 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+			Scanner s = new Scanner(System.in);
+		int a = s.nextInt();
+		int b = s.nextInt();
+		if(b>a) {
+			System.out.println("YES");
+		}
+		else {
+			System.out.println("NO");
+		}
+
+	}
+}
+
+//----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+// Input--
+// 7 9   6 6
+// Output--
+// YES    NO
