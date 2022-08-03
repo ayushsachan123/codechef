5e264442f1a80ed43e5dcb006d8a02c95ce82ea6
ayushsachan123 <97084090+ayushsachan123@users.noreply.github.com>
2022-08-03 21:47:04 +0530

    Create CHAIRS_.java

---
+++ JUNE221D/CHAIRS_.java
@@ -0,0 +1,41 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner s = new Scanner(System.in);
+		int  n = s.nextInt();
+		while(n-->0) {
+			int  a = s.nextInt();
+			int  b = s.nextInt();
+		  if(a>=b) {
+			  System.out.println(a-b);
+		  }
+		  else if(a<b) {
+			  System.out.println("0");
+		  }
+		}
+	}
+}
+
+
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+// Input--
+// 4
+// 20 14
+// 41 41
+// 35 0
+// 50 100
+  
+// Output--
+// 6
+// 0
+// 35
+// 0
