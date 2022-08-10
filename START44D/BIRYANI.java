8d82c5557031c608c0f1c20b12e30600a9a0e79d
ayushsachan123 <97084090+ayushsachan123@users.noreply.github.com>
2022-08-10 23:10:46 +0530

    Update BIRYANI.java

--- START44D/BIRYANI.java
+++ START44D/BIRYANI.java
@@ -1,11 +1,7 @@
-/* package codechef; // don't place package name! */
-
 import java.util.*;
 import java.lang.*;
 import java.io.*;
 
-/* Name of the class has to be "Main" only if the class is public. */
-class Codechef
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
@@ -23,7 +19,7 @@ class Codechef
 
 //----------------------------------------------------------------------------------------------------------------------------------------------------------------------
 // Input--
-// 4
+// 4. 
 // 1 10
 // 1 15
 // 2 10
